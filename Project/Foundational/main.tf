ecc6908cb698795275d5ea62484dfe3d4f94929b
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-06-19 01:00:47 -0400

    Updated changes to terraform file

--- Project/Foundational/main.tf
+++ Project/Foundational/main.tf
@@ -191,19 +191,19 @@ resource "aws_elb" "example" {
     name = "project-terraform-elb"
     internal = false
     availability_zones = ["us-east-1a", "us-east-1b"]
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
-    interval = 30
-    target = "HTTP:8080/"
-  }
-  listener = {
-    instance_port     = "80"
-    instance_protocol = "HTTP"
-    lb_port           = "80"
-    lb_protocol       = "HTTP"
-  }
+    listener = [{
+        instance_port     = "80"
+        instance_protocol = "HTTP"
+        lb_port           = "80"
+        lb_protocol       = "HTTP"
+    }]
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        timeout = 3
+        interval = 30
+        target = "HTTP:8080/"
+    }
   cross_zone_load_balancing   = true
   idle_timeout                = 400
   connection_draining         = true
