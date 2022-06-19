f3b56e8975cf3be329ddfa9fe3eda4188bc70a78
Jason Ceballos <30805605+jasonceballos29@users.noreply.github.com>
2022-06-19 01:39:09 -0400

    Continued updates to the terraform file

--- Project/Foundational/main.tf
+++ Project/Foundational/main.tf
@@ -74,11 +74,11 @@ resource "aws_subnet" "pubsub_2" {
     ]
     }
 # Create the DB Server private subnets
-resource "aws_subnet" "projectdb_subnet1" {
+resource "aws_subnet" "db_subnet1" {
     tags = {
-        Name = "DBSubnet" 
+        Name = "DBSubnet1" 
     }
-    vpc_id = aws_subnet.pubsub_1.id
+    vpc_id = aws_vpc.project_vpc.id
     cidr_block = "10.0.3.0/24"
     map_public_ip_on_launch = false
     availability_zone = "us-east-1b"
@@ -86,11 +86,11 @@ resource "aws_subnet" "projectdb_subnet1" {
       aws_vpc.project_vpc
     ]
 }
-resource "aws_subnet" "projectdb_subnet2" {
+resource "aws_subnet" "db_subnet2" {
     tags = {
         Name = "DBSubnet2" 
     }
-    vpc_id = aws_subnet.pubsub_1.id
+    vpc_id = aws_vpc.project_vpc.id
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = false
     availability_zone = "us-east-1a"
@@ -103,18 +103,14 @@ resource "aws_db_subnet_group" "db-subnet" {
     name = "db_subnet"
     subnet_ids = ["${aws_subnet.projectdb_subnet1.id}", "${aws_subnet.projectdb_subnet2.id}"]
 }      
-# Define routing table for Public Subnets
+# Attach routing table to Internet Gateway
 resource "aws_route_table" "project_routingtable" {
     vpc_id = aws_vpc.project_vpc.id
 
     route {
-        cidr_block = "10.0.1.0/24"
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.project_IG.id
     }
-    route {
-        cidr_block = "10.0.2.0/24"
-        gateway_id = aws_internet_gateway.project_IG.id
-  }
     tags = {
         Name = "project_routingtable"
     }
